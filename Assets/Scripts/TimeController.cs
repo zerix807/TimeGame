47056a90eae97f37d2df55eee45f48ee8ffa3026
zerix807 <u0815533@1110mac-3.lib.utah.edu>
2015-10-22 14:26:43 -0600

    time controller config
    time controller config

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class TimeController : MonoBehaviour {
 
 	public float timeSlow = 0.05f;
-	public float timeFast = 0.05f;
+	public float timeFast = 0.1f;
 	public float speedLimit = 0.01f;
 	// Update is called once per frame
 	void Update () {
